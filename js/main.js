52ecf4cbdc7e225475b03b248f4dfc900c9c48bf
Beth Williams <bwilliams1014@gmail.com>
2018-03-25 15:11:55 -0400

    Set up event listener in javascript file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 
-document.getElementById('firstName').addEventListener('submit', function() {
-	console.log('text');
+var theClick = document.getElementById('theForm');
+var theFirst = document.getElementById('firstName');
+var theLast = document.getElementById('lastName');
+var theEmail = document.getElementById('email');
+var theMessage = document.getElementById('message')
+
+
+theClick.addEventListener('submit', function(eventInfo) {
+	eventInfo.preventDefault();
+	console.log('The first name is: ');
+	console.log('The last name is: ');
+	console.log('The email is: ');
+	console.log('The message is: ');
+
 });
 
