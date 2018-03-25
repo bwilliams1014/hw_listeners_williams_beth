754b806130def88028f969eb68fac2068c035380
Beth Williams <bwilliams1014@gmail.com>
2018-03-25 12:05:49 -0400

    Fixed output so number is on same line as text

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ var count = 1;
 
 clickMe.addEventListener('click', function() {
 	var newParagraph = document.createElement('p');
-	newParagraph.innerHTML = '<p>This is click number </p>' + count;
+	var newText = document.createTextNode("This is click number " + count)
+//	newParagraph.innerHTML = '<p>This is click number </p>' + count;
+	newParagraph.appendChild(newText)
 	document.getElementsByTagName('body')[0].appendChild(newParagraph);
 	count++;
 })
+
+function myFunction() {
+    var para = document.createElement("P");
+    var t = document.createTextNode("This is a paragraph.");
+    para.appendChild(t);
+    document.getElementById("myDIV").appendChild(para);
+}
